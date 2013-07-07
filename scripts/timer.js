4fdb50566245da7d02566ba74653d316094729a5
Samuel Winchester <samuel@debian>
2013-07-07 11:11:07 +0800

    add AnimationTimer but its stop() maybe useless.

--- scripts/timer.js
+++ scripts/timer.js
@@ -26,12 +26,8 @@ function Timer(runnable, interval) {
 	this.isRunning = false;
 	if(this.runnable = Runnable.wrap(runnable))
 		this.worker = function() {
-			if(me.isRunning) {
-				if(me.runnable.run())
-					me.handle = window.setTimeout(me.worker, me.interval);
-				else
-					me.stop();
-			}
+			if(me.isRunning && me.runnable.run())
+				me.handle = window.setTimeout(me.worker, me.interval);
 			else
 				me.stop();
 		};
@@ -49,6 +45,46 @@ Timer.prototype.stop = function() {
 	this.isInteruppted = true;
 	if(this.handle !== undefined && this.handle !== null) {
 		window.clearTimeout(this.handle);
+		this.handle = null;
+	}
+}
+
+function AnimationTimer(runnable) {
+	var requestAnimationFrameImpl = window.requestAnimationFrame
+		|| window.mozRequestAnimationFrame
+		|| window.webkitRequestAnimationFrame
+		|| window.msRequestAnimationFrame;
+	var cancelAnimationFrameImpl = window.cancelAnimationFrame;
+	if(!requestAnimationFrameImpl)
+		throw new Error("requestAnimationFrame is not supported");
+	if(!(this.runnable = Runnable.wrap(runnable)))
+		throw new Error("can't make a runnable object from " + runnable);
+	RootObject.call(this);
+	var me = this;
+	this.handle = undefined;
+	this.isRunning = false;
+	this.requestAnimationFrame = requestAnimationFrameImpl;
+	this.cancelAnimationFrame = cancelAnimationFrameImpl;
+	this.worker = function() {
+		if(me.isRunning && me.runnable.run())
+			me.handle = me.requestAnimationFrame.call(null, me.worker);
+		else
+			me.stop();
+	};
+}
+AnimationTimer.prototype = new RootObject();
+AnimationTimer.prototype.start = function() {
+	if(this.isRunning)
+		return;
+	this.isRunning = true;
+	this.worker();
+}
+AnimationTimer.prototype.stop = function() {
+	this.isRunning = false;
+	this.isInteruppted = true;
+	if(this.handle !== undefined && this.handle !== null) {
+		if(this.cancelAnimationFrame)
+			this.cancelAnimationFrame.call(null, this.handle);
 		this.handle = null;
 	}
 }
